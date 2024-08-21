deb870c4bcc2abd7b5e392228b025542e400057b
dison0331 <139662128+dison0331@users.noreply.github.com>
2024-08-21 11:58:57 +0800

    gengxin

--- test-2-ai.py
+++ test-2-ai.py
@@ -34,5 +34,5 @@ if __name__ == "__main__":
     # 设置事件通道的监听意图
     intents = botpy.Intents(public_guild_messages=True)
     client = MyClient(intents=intents)
-    # 运行机器人，传入appid和token
+    # 运行机器人，传入appid和token.
     client.run(appid=test_config["appid"], token=test_config["token"])
